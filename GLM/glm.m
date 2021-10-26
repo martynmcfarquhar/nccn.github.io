ee6d0d2b5e98d6672e314f119e3af32607e1f709
Martyn McFarquhar <martynmcfarquhar@googlemail.com>
2021-10-26 08:31:01 +0100

    Updated GLM code

--- GLM/glm.m
+++ GLM/glm.m
@@ -1,3 +1,4 @@
+% Raw data
 Y = [21 21 22.8 21.4 18.7 18.1]';
 
 X = [1 110 2.620 1; ...
@@ -6,7 +7,8 @@ X = [1 110 2.620 1; ...
      1 110 3.215 0; ...
      1 175 3.440 0; ...
      1 105 3.460 0];
- 
+
+% Estimate GLM 
 Beta        = inv(X' * X) * X' * Y; 
 Yhat        = X * Beta;
 Residuals   = Y - Yhat;
@@ -14,14 +16,14 @@ Sigma2      = (Residuals' * Residuals) / (size(X,1) - size(X,2));
 Vcov        = Sigma2 * inv(X' * X);
 SE          = sqrt(diag(Vcov));
 
+% Results table
 Effects = {'Constant';'Horsepower';'Weight';'Transmission'};
 Estimate = round(Beta,3);
 StandardError = round(SE,3);
-
 table(Estimate,StandardError,'RowNames',Effects)
 
-% Statistics toolbox needed below here
-figure('Position', [100 100 1024 300])
+% Plot results (Statistics toolbox needed)
+figure('Position', [100 100 1050 275])
 for i = 1:3
     subplot(1,3,i);
     addedvarplot(X(:,2:4),Y,i,[true true true]);
